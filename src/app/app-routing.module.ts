958aeff9e10c08514f2fa53a8feabfc6f46323df
Development456 <120048356+Development456@users.noreply.github.com>
2023-01-19 11:46:59 -0500

    Update app-routing.module.ts
    import signup issue fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "./components/login/login.component";
+import { SignUpComponent } from "./components/login/sign-up/sign-up.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component"
 import { AuthGuard } from './auth.guard';
 
