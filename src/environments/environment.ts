dddd99c913c31c8294d01db4b93ae2934a3ba39e
Mansi0899 <48039641+Mansi0899@users.noreply.github.com>
2023-01-23 16:25:56 -0500

    env-chnages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,14 +2,15 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
   production: false,
   // URL:'http://172.174.113.233:8080'
-  CLAIM:'http://172.174.113.233:9000',
-  FACILITY:'http://172.174.113.233:9001',
-  CUSTOMER:'http://172.174.113.233:9002',
-  LOGIN:'http://172.174.113.233:9002',
-  API:'http://172.174.113.233:9090'
+  CLAIM:'http://localhost:8100',
+  FACILITY:'http://localhost:8200',
+  CUSTOMER:'http://localhost:8400',
+  LOGIN:'http://localhost:8300',
+  API: 'http://localhost:8500'
 };
 
 /*
