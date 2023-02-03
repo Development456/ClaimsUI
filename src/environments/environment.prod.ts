5b30cfceccc6887a631cf24427d19c08bc067d2c
Apurva Bansode <45214076+apurva199@users.noreply.github.com>
2023-02-03 13:42:23 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,8 +5,5 @@ export const environment = {
     FACILITY:'http://172.174.113.233:9001',
     CUSTOMER:'http://172.174.113.233:9002',
     LOGIN:'http://172.174.113.233:9003',
-    API:'http://172.174.113.233:9090',
-    AUTH_URL: 'http://172.174.113.233:9002'
-
-  
+    API:'http://172.174.113.233:9090'
 };
