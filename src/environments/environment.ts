7a9589adc22481988d28895765fa6822d19739d1
Mansi0899 <48039641+Mansi0899@users.noreply.github.com>
2023-02-14 15:24:44 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,19 +5,16 @@
 
 export const environment = {
   production: false,
-  URL:'http://172.174.113.233:8080',
-//   CLAIM:'http://20.62.171.46:9000',
-// FACILITY:'http://20.62.171.46:9001',
-// CUSTOMER:'http://20.62.171.46:9002',
-// LOGIN:'http://20.62.171.46:9003',
-// API:'http://20.62.171.46:9090',
-  CLAIM: 'http://localhost:8100',
-  FACILITY: 'http://localhost:8200',
-  CUSTOMER: 'http://localhost:8400',
-  LOGIN: 'http://localhost:8300',
-  AUTH_URL: 'http://172.174.113.233:9002',
-  API: 'http://localhost:8500'
-
+    CLAIM:'http://20.62.171.46:9000',
+FACILITY:'http://20.62.171.46:9001',
+CUSTOMER:'http://20.62.171.46:9002',
+LOGIN:'http://20.62.171.46:9003',
+API:'http://20.62.171.46:9090',
+  // CLAIM:'http://localhost:8100',
+  // FACILITY:'http://localhost:8200',
+  // CUSTOMER:'http://localhost:8400',
+  // LOGIN:'http://localhost:8300',
+  // API: 'http://localhost:8500'
 };
 
 /*
